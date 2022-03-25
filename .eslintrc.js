87f1d4ac1c09837f8c30eef88a9f606bf8287311
君惜 <xinlei3166@126.com>
2022-03-25 16:28:09 +0800

    chore: update package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+       ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
       tsx: true,
